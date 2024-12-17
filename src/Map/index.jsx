b9167fb37448fd56e4af667c3bc6e323d2a9e451
Jorke Odolphi <jorkeo@amazon.com>
2024-12-17 22:01:07 +1100

    update to react 19, maplibre pre 10, visgl beta pre

--- src/Map/index.jsx
+++ src/Map/index.jsx
@@ -6,7 +6,6 @@ import MapGL, {
   Layer,
 } from '@vis.gl/react-maplibre'
 import maplibregl from 'maplibre-gl'
-// import * as pmtiles from 'pmtiles'
 
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './index.css'
@@ -14,29 +13,16 @@ import LatLonBox from '../LatLonBox'
 import StyleSelector from '../StyleSelector'
 
 const apiKey = process.env.APIKEY || ''
-const defaultMapStyle = process.env.DEFAULT_MAPSTYLE || 'satmap'
+const defaultMapStyle = process.env.DEFAULT_MAPSTYLE || 'Hybrid'
 const region = process.env.REGION || ''
+const colorScheme = 'Light'
 
 const Map = () => {
-
-  const [pmTilesReady, setPmTilesReady] = useState(false)
-  
   const [viewport, setViewport] = useState({
     latitude: -33.875,
     longitude: 151.205,
     zoom: 5,
   })
-
-  // useEffect(() => {
-  //   const protocol = new pmtiles.Protocol({metadata: true})
-  //   maplibregl.addProtocol('pmtiles', protocol.tile)
-  //   setPmTilesReady(true)
-    
-  //   return () => {
-  //     maplibregl.removeProtocol('pmtiles')
-  //   }
-
-  // }, [])
 
   const [lngLat, setLngLat] = useState({ lat: viewport.latitude, lng: viewport.longitude})
 
@@ -49,23 +35,12 @@ const Map = () => {
           initialViewState={viewport} 
           ref={mapRef}
           onViewportChange={(v) => setViewport(v)}
-          width="100vw"
-          height="100vh"
-          // mapStyle={pmTilesReady ? `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${defaultMapStyle}/style-descriptor?key=${apiKey}` : undefined}
-          mapStyle={`https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${defaultMapStyle}/style-descriptor?key=${apiKey}`}
+          mapStyle={`https://maps.geo.${region}.amazonaws.com/v2/styles/${defaultMapStyle}/descriptor?color-scheme=${colorScheme}&key=${apiKey}`}
           onMouseMove={(m) =>setLngLat(m.lngLat)}
           tileSize={512}
           projection={{ type: 'mercator'}}
           mapLib={maplibregl}
         >
-          {/* <Source id="pmtiles" type="vector" url="pmtiles://https://d1firxt62yjjug.cloudfront.net/pmtiles/nsw_lga.pmtiles"> 
-            <Layer id="pmtiles-lines" source="pmtiles" type="line" source-layer="nswlgaboundaries"
-              paint={{
-                "line-color": "red",
-                "line-width": 2
-              }}
-            />
-          </Source> */}
           <GeolocateControl
             style={{ right: 10, bottom: 85 }}
             positionOptions={{ enableHighAccuracy: true }}
